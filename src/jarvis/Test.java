13fa44b9dc22c765a408e834138c4c877a564cd7
hyosang <hyosang@hyosang-PC>
2015-05-26 00:28:41 +0900

    Komoran 형태소 분석기를 사용하기 위해 테스트함.

--- src/jarvis/Test.java
+++ src/jarvis/Test.java
@@ -1,22 +1,41 @@
 package jarvis;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import kr.co.shineware.nlp.komoran.core.analyzer.Komoran;
+import kr.co.shineware.util.common.model.Pair;
 
 public class Test {
 
 	public static void main(String[] args) {
-//		RunningApplications ra = new RunningApplications();
-//		ra.runMemoPad();
-//		try {
-//			// 바로 시작하면 동시에 쓰레드가 실행되어 마지막 인자만 실행되어 진다.
-//			// 이를 방지하기 위해 몇초정도 늦게 프로그램을 실행하도록 한다.
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		ra.runCalculator();
 
+		String message = "네이버에서 삼성 검색";
+		Komoran komoran = new Komoran("./models/models-full/");
+
+		List<List<Pair<String, String>>> result = komoran.analyze(message);
+
+		for (List<Pair<String, String>> eojeolResult : result) {
+			for (Pair<String, String> wordMorph : eojeolResult) {
+
+				System.out.println(wordMorph.getFirst());
+				System.out.println(wordMorph.getSecond());
+			}
+		}
+		
+		System.out.println("===========================");
+		
+		message = "음악 틀어줘";
+
+
+		result = komoran.analyze(message);
+		for (List<Pair<String, String>> eojeolResult : result) {
+			for (Pair<String, String> wordMorph : eojeolResult) {
+
+				System.out.println(wordMorph.getFirst());
+				System.out.println(wordMorph.getSecond());
+			}
+		}
 	}
 
 }
