5a9c3e1726584ae71be18e49331a43b2d911433c
hyosang <hyosang@hyosang-PC>
2015-05-25 19:05:29 +0900

    테스트용 클래스 추가.

---
+++ src/jarvis/Test.java
@@ -0,0 +1,22 @@
+package jarvis;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+//		RunningApplications ra = new RunningApplications();
+//		ra.runMemoPad();
+//		try {
+//			// 바로 시작하면 동시에 쓰레드가 실행되어 마지막 인자만 실행되어 진다.
+//			// 이를 방지하기 위해 몇초정도 늦게 프로그램을 실행하도록 한다.
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		ra.runCalculator();
+
+	}
+
+}
